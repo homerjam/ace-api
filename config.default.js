64fdb9a40d323da23e382d85d96dc3bf0e4c8684
James Homer <jameshomer85@gmail.com>
2017-12-07 17:36:39 +0000

    0.5.20

--- config.default.js
+++ config.default.js
@@ -6,7 +6,7 @@ const config = {
 
   cache: {
     enabled: process.env.CACHE_ENABLED || false,
-    maxSize: process.env.CACHE_MAX_SIZE || 128 * 1000 * 1000, // ~128mb
+    maxSize: process.env.CACHE_MAX_SIZE || 256 * 1000 * 1000, // ~128mb
     maxAge: process.env.CACHE_MAX_AGE || 30 * 60 * 1000, // 30mins
   },
 
