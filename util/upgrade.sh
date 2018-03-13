9897dde08d496e6f069b4fdd946a9d95c2828b5c
James Homer <jameshomer85@gmail.com>
2018-03-13 17:36:57 +0000

    fix entity merging

--- util/upgrade.sh
+++ util/upgrade.sh
@@ -8,9 +8,9 @@ for slug in ${array[@]}
 do
   # couchbackup --url $1 --db $slug > $(dirname "$(pwd)")/tmp/backup/$slug.backup
 
-  node update-entities $1 $slug
-  node update-files $1 $slug
+  # node update-entities $1 $slug
+  # node update-files $1 $slug
   # node update-ecom $1 $slug
-  node ../design $1 $slug
+  node ../design $1 entity $slug
   node update-index $1 $slug
 done
