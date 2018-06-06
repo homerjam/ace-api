6e87c1e336133f6ff59b94af82631e17d55615a1
James Homer <jameshomer85@gmail.com>
2018-06-06 19:22:03 +0100

    add provider/api route

---
+++ server/routes/provider.js
@@ -0,0 +1,88 @@
+const _ = require('lodash');
+const axios = require('axios');
+
+module.exports = ({
+  Auth,
+  ClientConfig,
+  router,
+  cacheMiddleware,
+  asyncMiddleware,
+  getConfig,
+  handleResponse,
+  handleError,
+}) => {
+
+  const providerApiBaseUrl = {
+    google: 'https://www.googleapis.com',
+    instagram: 'https://api.instagram.com',
+    spotify: 'https://api.spotify.com',
+    vimeo: 'https://api.vimeo.com',
+  };
+
+  const providerApiHandler = asyncMiddleware(async (req, res) => {
+    const method = req.method;
+    const provider = req.params[0];
+    const userId = req.params[2] ? req.params[1] : null;
+    const endpoint = (req.params[2] || req.params[1]).split('/').filter(param => param !== '').join('/');
+
+    const config = await getConfig(req.session.slug);
+
+    const cc = ClientConfig(config);
+
+    let clientConfig = await cc.get();
+    let providerConfig;
+
+    if (userId) {
+      providerConfig = clientConfig.userSettings[userId].provider[provider];
+    } else {
+      providerConfig = clientConfig.provider[provider];
+    }
+
+    if (Math.floor(new Date().getTime() / 1000) - (providerConfig.begins || 0) > providerConfig.expires_in) {
+      const auth = Auth(await getConfig(req.session.slug));
+
+      if (userId) {
+        clientConfig = await auth.authProvider(provider, {}, userId, true);
+        providerConfig = clientConfig.userSettings[userId].provider[provider];
+      } else {
+        clientConfig = await auth.authProvider(provider, {}, null, true);
+        providerConfig = clientConfig.provider[provider];
+      }
+    }
+
+    const params = _.merge({}, req.query);
+
+    if (!/bearer/i.test(providerConfig.token_type)) {
+      params.access_token = providerConfig.access_token;
+    }
+
+    try {
+      const result = await axios.request({
+        url: endpoint,
+        baseURL: providerApiBaseUrl[provider],
+        method,
+        headers: {
+          Authorization: `Bearer ${providerConfig.access_token}`,
+        },
+        params,
+      });
+
+      handleResponse(req, res, result.data, true);
+    } catch (error) {
+      handleError(req, res, error);
+    }
+  });
+
+  router.all(
+    /\/provider\/([^/]+)\/([^/]+)\/api\/?(.+)?/,
+    cacheMiddleware,
+    providerApiHandler,
+  );
+
+  router.all(
+    /\/provider\/([^/]+)\/api\/?(.+)?/,
+    cacheMiddleware,
+    providerApiHandler,
+  );
+
+};
