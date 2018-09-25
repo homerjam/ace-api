4642fdcf9486ab616d4fba9d5694f2396335c9c9
James Homer <jameshomer85@gmail.com>
2018-09-25 16:10:35 +0100

    fix delete entity

--- design/entity/views/byChildren.js
+++ design/entity/views/byChildren.js
@@ -1,36 +1,31 @@
 /* global emit */
 
-module.exports = function (doc) {
+module.exports = function(doc) {
   if (doc.type === 'entity') {
-
     function type(obj) {
       return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
     }
 
     function forEach(obj, fn) {
-      Object.keys(obj).forEach(function (key) {
+      Object.keys(obj).forEach(function(key) {
         fn(obj[key], key, obj);
       });
     }
 
     forEach(doc.fields, function(field) {
-
-      if (!type(field.value) === 'array') {
+      if (type(field.value) !== 'array') {
         return;
       }
 
       forEach(field.value, function(obj) {
-        if (!type(obj) === 'object') {
+        if (type(obj) !== 'object') {
           return;
         }
 
         if (obj.type === 'entity') {
-          emit(obj.id, {
-            _id: doc._id,
-          });
+          emit(obj.id, { _id: doc._id });
         }
       });
-
     });
   }
 };
