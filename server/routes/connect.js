f5246266a7cde5d0cebb8b32b1becea9a68135a8
James Homer <james@homerjam.es>
2020-03-03 23:55:30 +0000

    providers

--- server/routes/connect.js
+++ server/routes/connect.js
@@ -16,7 +16,32 @@ module.exports = async ({ app, getConfig }) => {
         secret: config.provider.google.clientSecret,
         scope: ['openid', 'profile', 'email'],
         nonce: true,
-        custom_params: { access_type: 'offline', referer: req.get('Referer') },
+        custom_params: { access_type: 'offline' },
+        callback: config.cms.url + '/connect',
+      },
+      instagram: {
+        key: config.provider.instagram.clientId,
+        secret: config.provider.instagram.clientSecret,
+        scope: ['basic'],
+        nonce: true,
+        callback: config.cms.url + '/connect',
+      },
+      spotify: {
+        key: config.provider.spotify.clientId,
+        secret: config.provider.spotify.clientSecret,
+        scope: [
+          'user-read-email',
+          'user-top-read',
+          'user-read-recently-played',
+        ],
+        nonce: true,
+        callback: config.cms.url + '/connect',
+      },
+      vimeo: {
+        key: config.provider.vimeo.clientId,
+        secret: config.provider.vimeo.clientSecret,
+        scope: ['public', 'private', 'video_files'],
+        nonce: true,
         callback: config.cms.url + '/connect',
       },
     })(req, res, next);
